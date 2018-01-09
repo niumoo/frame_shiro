c97338f5334d89617e1365379d0cfd9932ea06b3
niumoo-thinkpad <niumoo@126.com>
2018-01-09 15:58:23 +0800

    shiro-spring-web：与spring的整合

---
+++ shiro-spring/src/main/java/me/imniu/shiro_spring/service/PermissionService.java
@@ -0,0 +1,13 @@
+package me.imniu.shiro_spring.service;
+
+import me.imniu.shiro_spring.entity.Permission;
+
+/**
+ * <p>User: Zhang Kaitao
+ * <p>Date: 14-1-28
+ * <p>Version: 1.0
+ */
+public interface PermissionService {
+    public Permission createPermission(Permission permission);
+    public void deletePermission(Long permissionId);
+}
---
+++ shiro-spring/src/main/java/me/imniu/shiro_spring/service/PermissionServiceImpl.java
@@ -0,0 +1,26 @@
+package me.imniu.shiro_spring.service;
+
+import me.imniu.shiro_spring.dao.PermissionDao;
+import me.imniu.shiro_spring.entity.Permission;
+
+/**
+ * <p>User: Zhang Kaitao
+ * <p>Date: 14-1-28
+ * <p>Version: 1.0
+ */
+public class PermissionServiceImpl implements PermissionService {
+
+    private PermissionDao permissionDao;
+
+    public void setPermissionDao(PermissionDao permissionDao) {
+        this.permissionDao = permissionDao;
+    }
+
+    public Permission createPermission(Permission permission) {
+        return permissionDao.createPermission(permission);
+    }
+
+    public void deletePermission(Long permissionId) {
+        permissionDao.deletePermission(permissionId);
+    }
+}
